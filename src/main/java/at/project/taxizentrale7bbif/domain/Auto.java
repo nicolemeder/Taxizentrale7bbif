32d59d3b5058cc3ce234b54284c915c6b5a0cb96
nico rosenfeld <ros19255@spengergasse.at>
2022-01-30 11:35:09 +0100

    Domainclass Adresse edited Domainclass Auto edited

--- src/main/java/at/project/taxizentrale7bbif/domain/Auto.java
+++ src/main/java/at/project/taxizentrale7bbif/domain/Auto.java
@@ -1,14 +1,20 @@
 package at.project.taxizentrale7bbif.domain;
 
+import at.project.taxizentrale7bbif.persistence.converters.TaxiTypConverter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import javax.persistence.*;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
 import java.math.BigInteger;
 import java.time.LocalDate;
+import java.time.Year;
 
 @Data
 @NoArgsConstructor
@@ -19,27 +25,44 @@ import java.time.LocalDate;
 @Table(name = "autos")
 public class Auto extends AbstractPersistable<Long> {
 
-    private int baujahr;
-    private int kmStand;
+    @Version
+    private Integer version;
+    @PastOrPresent
+    private Year baujahr;
+    @Positive
+    private Integer kmStand;
     private BigInteger einnahmen;
+    @Future
     private LocalDate serviceFaelligkeitsdatum;
-    private int ps;
+    @Positive
+    private Integer ps;
 
 
-    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, optional = false)
+    @JoinColumn(foreignKey = @ForeignKey(name = "FK_Auto_2_Taxizentrale"))
     private Taxizentrale zentrale;
 
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(foreignKey = @ForeignKey(name = "FK_Auto_2_Marke"))
     private Marke marke;
 
 
-
-    @Enumerated(EnumType.STRING)
+    //@Enumerated(EnumType.STRING)
+    @Convert(converter = TaxiTypConverter.class) //converts it to a database value
+    @Column(columnDefinition = "char(1) check (taxityp in ('N', 'B', 'G', 'V')))") //check constraint
     private TaxiTyp taxiTyp;
 
 
+    public boolean hasZentrale()
+    {
+        return this.zentrale != null;
+    }
 
-
+    public Auto clearZentrale()
+    {
+        this.zentrale = null;
+        return this;
+    }
 
 
 }
