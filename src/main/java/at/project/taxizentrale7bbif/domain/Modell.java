e8392e40e7341bff2d6c6dfb593c5bbcb68e0495
nico rosenfeld <ros19255@spengergasse.at>
2022-01-30 11:36:47 +0100

    Domainclass Modell edited Domainclass TaxiTyp edited

--- src/main/java/at/project/taxizentrale7bbif/domain/Modell.java
+++ src/main/java/at/project/taxizentrale7bbif/domain/Modell.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
@@ -17,8 +20,16 @@ import javax.persistence.*;
 @Table(name = "modelle")
 public class Modell extends AbstractPersistable<Long> {
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String name;
 
     @OneToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(foreignKey = @ForeignKey(name = "FK_Modell_2_Marke"))
     private Marke marke;
+
+
+
+
 }
--- src/main/java/at/project/taxizentrale7bbif/domain/TaxiTyp.java
+++ src/main/java/at/project/taxizentrale7bbif/domain/TaxiTyp.java
@@ -1,9 +1,25 @@
 package at.project.taxizentrale7bbif.domain;
 
 
+import lombok.Getter;
+
+import java.util.Arrays;
+
 public enum TaxiTyp {
-    NICHTRAUCHER,
-    BEHINDERTENTRANSPORT,
-    GROSSRAUM,
-    VIP
+    NICHTRAUCHER("N"),
+    BEHINDERTENTRANSPORT("B"),
+    GROSSRAUM("G"),
+    VIP("V");
+
+    @Getter
+    private final String databaseValue;
+
+    TaxiTyp(String databaseValue) {
+        this.databaseValue = databaseValue;
+    }
+
+    public static TaxiTyp forDatabaseValue(String databaseValue) {
+        return Arrays.stream(values()).filter(taxityp -> taxityp.databaseValue.equalsIgnoreCase(databaseValue))
+                .findFirst().orElseThrow(() -> new IllegalArgumentException(String.format("Ungültiger Wert.")));
+    }
 }
