260b1e1814d90af08b0eac2dc4b359f248d56c36
NMeder <med20046@spengergasse.at>
2022-01-30 02:11:13 +0100

    bug fix

--- src/main/java/at/project/taxizentrale7bbif/persistence/TaxizentraleRepository.java
+++ src/main/java/at/project/taxizentrale7bbif/persistence/TaxizentraleRepository.java
@@ -2,11 +2,18 @@ package at.project.taxizentrale7bbif.persistence;
 
 import at.project.taxizentrale7bbif.domain.Taxizentrale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface TaxizentraleRepository extends JpaRepository<Taxizentrale, Long> {
+public interface TaxizentraleRepository extends JpaRepository<Taxizentrale, Long>/*,QuerydslPredicateExecutor<Taxizentrale>*/ {
+                                                                                    //dynamic queries
+                                                                                    //couldn't find QTaxizentrale-->error
+    Optional<Taxizentrale> findByName(String name);
 
-
+    List<Taxizentrale> findByNameLike(String name);
 
 }
