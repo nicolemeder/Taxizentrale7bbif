2a9af4113131ef82a926f7df83d8ef688fdcc137
nico rosenfeld <ros19255@spengergasse.at>
2022-01-30 11:35:59 +0100

    AutoRepository edited

--- src/main/java/at/project/taxizentrale7bbif/persistence/AutoRepository.java
+++ src/main/java/at/project/taxizentrale7bbif/persistence/AutoRepository.java
@@ -4,6 +4,11 @@ import at.project.taxizentrale7bbif.domain.Auto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AutoRepository extends JpaRepository<Auto, Long>{
+    List<Auto> findAllByMarke_Name(String name);
 }
+
+//in the making
