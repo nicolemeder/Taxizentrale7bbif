f3329bcd4315dcd9b2f1d7e5ae2be141417ec066
nicolemeder <med20046@spengergasse.at>
2022-04-14 21:53:55 +0200

    CRUD methods added

--- src/main/java/at/project/taxizentrale7bbif/service/FahrtService.java
+++ src/main/java/at/project/taxizentrale7bbif/service/FahrtService.java
@@ -1,11 +1,14 @@
 package at.project.taxizentrale7bbif.service;
 
 import at.project.taxizentrale7bbif.domain.Fahrt;
+import at.project.taxizentrale7bbif.domain.Mitarbeiter;
 import at.project.taxizentrale7bbif.persistence.FahrtRepository;
+import at.project.taxizentrale7bbif.service.dtos.MutateFahrtCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,5 +20,61 @@ public class FahrtService {
     {
         return fahrtRepository.findAll();
     }
+
+    public Optional<Fahrt> getFahrt(Long id) { return fahrtRepository.findById(id); }
+
+    public Fahrt createFahrt(MutateFahrtCommand command) {
+        Fahrt fahrt = Fahrt.builder()
+                .nummer(command.getNummer())
+                .taxifahrer(Mitarbeiter.builder()
+                        .vorname(command.getVorname())
+                        .nachname(command.getNachname())
+                        .build())
+                .build();
+        return fahrtRepository.save(fahrt);
+    }
+
+    public Fahrt replaceFahrt(Long id, MutateFahrtCommand command) {
+        Optional<Fahrt> entity = fahrtRepository.findById(id);
+
+        if(entity.isPresent()) {
+            Fahrt fahrt = entity.get();
+            fahrt.setNummer(command.getNummer());
+            fahrt.getTaxifahrer().setVorname(command.getVorname());
+            fahrt.getTaxifahrer().setNachname(command.getNachname());
+
+            return fahrtRepository.save(fahrt);
+        }
+        throw new IllegalStateException("Diese Fahrt mit der ID '" +id+ "' kann nicht gefunden werden.");
+    }
+
+    public Fahrt partiallyUpdateFahrt(Long id, MutateFahrtCommand command) {
+        Optional<Fahrt> entity = fahrtRepository.findById(id);
+
+        if(entity.isPresent()) {
+            Fahrt fahrt = entity.get();
+
+            if (command.getNummer() != null) fahrt.setNummer((command.getNummer()));
+            if (command.getVorname() != null) fahrt.getTaxifahrer().setVorname(command.getVorname());
+            if (command.getNachname() != null) fahrt.getTaxifahrer().setNachname(command.getNachname());
+
+            return fahrtRepository.save(fahrt);
+        }
+        throw new IllegalStateException("Diese Fahrt mit der ID '" +id+ "' kann nicht gefunden werden.");
+    }
+
+    public void deleteFahrten() { fahrtRepository.deleteAll(); }
+
+    public void deleteFahrt(Long id) { fahrtRepository.deleteById(id);}
+
+
+
+
+
+
+
+
+
+
 
 }
