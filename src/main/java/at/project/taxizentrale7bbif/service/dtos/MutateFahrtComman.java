43e95abde12a2a346a8a212385d00224ec0be80d
nicolemeder <med20046@spengergasse.at>
2022-04-14 21:07:14 +0200

    created

---
+++ src/main/java/at/project/taxizentrale7bbif/service/dtos/MutateFahrtComman.java
@@ -0,0 +1,13 @@
+package at.project.taxizentrale7bbif.service.dtos;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MutateFahrtComman {
+    private Integer nummer;
+    private String vorname;
+    private String nachname;
+}
