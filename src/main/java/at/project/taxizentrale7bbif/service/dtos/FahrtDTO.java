e2016dfbbbbd0fe25f10804b2a27d678ca4b9952
nicolemeder <med20046@spengergasse.at>
2022-04-14 22:22:09 +0200

    created FahrtDTO

--- src/main/java/at/project/taxizentrale7bbif/service/dtos/FahrtDTO.java
+++ src/main/java/at/project/taxizentrale7bbif/service/dtos/FahrtDTO.java
@@ -2,9 +2,10 @@ package at.project.taxizentrale7bbif.service.dtos;
 
 import at.project.taxizentrale7bbif.domain.Fahrt;
 
-public record FahrtDTO(Integer fahrtNummer, MitarbeiterDTO taxifahrer) {
+public record FahrtDTO(Integer nummer, MitarbeiterDTO taxifahrer) {
     public FahrtDTO(Fahrt fahrt)
     {
         this(fahrt.getNummer(), new MitarbeiterDTO(fahrt.getTaxifahrer()));
     }
+
 }
