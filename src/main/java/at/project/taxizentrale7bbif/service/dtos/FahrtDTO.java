d571fbd0467db885ef894f4c8b9035bc25875a7f
nicolemeder <med20046@spengergasse.at>
2022-04-15 00:13:51 +0200

    minor change

--- src/main/java/at/project/taxizentrale7bbif/service/dtos/FahrtDTO.java
+++ src/main/java/at/project/taxizentrale7bbif/service/dtos/FahrtDTO.java
@@ -2,7 +2,7 @@ package at.project.taxizentrale7bbif.service.dtos;
 
 import at.project.taxizentrale7bbif.domain.Fahrt;
 
-public record FahrtDTO(Integer nummer, MitarbeiterDTO taxifahrer) {
+public record FahrtDTO(String nummer, MitarbeiterDTO taxifahrer) {
     public FahrtDTO(Fahrt fahrt)
     {
         this(fahrt.getNummer(), new MitarbeiterDTO(fahrt.getTaxifahrer()));
