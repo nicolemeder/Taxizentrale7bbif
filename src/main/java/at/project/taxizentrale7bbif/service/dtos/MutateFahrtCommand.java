66753dd8f5ad7dada836ad2225fe783ba2bf4579
nicolemeder <med20046@spengergasse.at>
2022-04-15 19:54:03 +0200

    new methode added

--- src/main/java/at/project/taxizentrale7bbif/service/dtos/MutateFahrtCommand.java
+++ src/main/java/at/project/taxizentrale7bbif/service/dtos/MutateFahrtCommand.java
@@ -1,13 +1,31 @@
 package at.project.taxizentrale7bbif.service.dtos;
 
 
+import at.project.taxizentrale7bbif.foundation.TokenSourceProvider;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 @Getter
 @NoArgsConstructor
-public class MutateFahrtCommand {
+public class MutateFahrtCommand implements TokenSourceProvider {
     private String nummer;
     private String vorname;
     private String nachname;
+
+
+@Override
+public List<Supplier<String>> getTokenRelevantFields() {
+    return List.of(
+            this::getNummer,
+            this::getVorname,
+            this::getNachname
+    );
+}
+
+
+
+
 }
