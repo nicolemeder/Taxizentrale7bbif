c65abc1bd85892d42d4ad8f4f91e9d0e378da67a
nicolemeder <med20046@spengergasse.at>
2022-04-17 21:30:28 +0200

    forms created

---
+++ src/main/java/at/project/taxizentrale7bbif/presentation/www/CreateMitarbeiterForm.java
@@ -0,0 +1,12 @@
+package at.project.taxizentrale7bbif.presentation.www;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CreateMitarbeiterForm {
+    private String vorname;
+    private String nachname;
+}
