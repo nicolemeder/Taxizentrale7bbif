974e55331b5a3e2e9271387a370398e8e3823c32
nicolemeder <med20046@spengergasse.at>
2022-04-17 17:16:32 +0200

    method added

--- src/main/java/at/project/taxizentrale7bbif/persistence/FahrtRepository.java
+++ src/main/java/at/project/taxizentrale7bbif/persistence/FahrtRepository.java
@@ -4,6 +4,9 @@ import at.project.taxizentrale7bbif.domain.Fahrt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FahrtRepository extends JpaRepository<Fahrt, Long> {
+    List<Fahrt> findByNummer(String nummer);
 }
